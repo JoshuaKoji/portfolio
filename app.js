aac70421551043a5efb062e84f1a0d58d79f03b7
Joshua Koji <joshuakoji@protonmail.com>
2023-02-21 13:55:06 -0500

    Update server code, add correct procfile

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    res.sendFile("index.html")
+})
+
+
+app.listen(process.env.PORT || 3000)
