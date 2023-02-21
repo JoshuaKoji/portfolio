4541dd273bef7cceab08f9d799276aed99e1762b
Joshua Koji <joshuakoji@protonmail.com>
2023-02-21 17:21:55 -0500

    fixed error in server with path name in sendfile

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-    res.sendFile("index.html")
+    res.sendFile(__dirname + "/index.html")
 })
 
 
